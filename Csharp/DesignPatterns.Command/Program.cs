eb973c21b1da71e508ac5e86c1a60a8c527b2ff2
ibrahimatay <contact@ibrahimatay.com>
2023-12-05 19:41:24 +0300

    DesignPatterns.Command

---
+++ Csharp/DesignPatterns.Command/Program.cs
@@ -0,0 +1,37 @@
+﻿var addCommand = new AddPostCommand();
+var updateCommand = new UpdatePostCommand();
+
+Controller controller = new Controller();
+
+controller.Invoke(addCommand);
+controller.Invoke(updateCommand);
+
+
+interface ICommand
+{
+    void Execute();
+}
+
+class Controller
+{
+    public void Invoke(ICommand command)
+    {
+        command.Execute();
+    }
+}
+
+class AddPostCommand : ICommand
+{
+    public void Execute()
+    {
+        Console.WriteLine("Added to Post");
+    }
+}
+
+class UpdatePostCommand : ICommand
+{
+    public void Execute()
+    {
+        Console.WriteLine("Updated to Post");
+    }
+}
