69dafac430e759a51f29cc3a3164cf0d18daf212
ibrahimatay <contact@ibrahimatay.com>
2023-12-22 17:18:54 +0300

    DesignPatterns.Strategy

---
+++ Csharp/DesignPatterns.Strategy/Program.cs
@@ -0,0 +1,44 @@
+﻿
+
+PluginManager pluginManager = new PluginManager(new DataManagementPlugin(), new EnginePlugin());
+pluginManager.Call();
+
+interface IPlugin
+{
+    void Call();
+}
+
+class PluginManager
+{
+    readonly List<IPlugin> _plugins;
+
+    public PluginManager(params IPlugin[] plugins)
+    {
+        _plugins = new List<IPlugin>();
+        _plugins.AddRange(plugins);
+    }
+
+    public void Call()
+    {
+        foreach (IPlugin plugin in _plugins)
+        {
+            plugin.Call();
+        }
+    }
+}
+
+class EnginePlugin : IPlugin
+{
+    public void Call()
+    {
+        Console.WriteLine("Engine Plugin Called");
+    }
+}
+
+class DataManagementPlugin : IPlugin
+{
+    public void Call()
+    {
+        Console.WriteLine("Data Management Plugin Called");
+    }
+}
