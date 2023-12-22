229808f572e963bae13b63d8cd85c47f7f0d6afa
ibrahimatay <contact@ibrahimatay.com>
2023-12-22 17:27:41 +0300

    DesignPatterns.TemplateMethod

---
+++ Csharp/DesignPatterns.TemplateMethod/Program.cs
@@ -0,0 +1,52 @@
+﻿BreadMaker wholeWheat = new WholeWheatBread();
+wholeWheat.Make();
+
+Console.WriteLine();
+
+BreadMaker grain = new GrainBread();
+grain.Make();
+
+
+abstract class BreadMaker
+{
+    protected abstract void Bake();
+    protected abstract void MixIngredients();
+
+    protected void Slice()
+    {
+        Console.WriteLine("Slicing the bread!");
+    }
+
+    public void Make()
+    {
+        MixIngredients();
+        Bake();
+        Slice();
+    }
+}
+
+class GrainBread : BreadMaker
+{
+    protected override void Bake()
+    {
+        Console.WriteLine("Baking the Grain Bread. (25 minutes)");
+    }
+
+    protected override void MixIngredients()
+    {
+        Console.WriteLine("Gathering Ingredients for Grain Bread.");
+    }
+}
+
+class WholeWheatBread : BreadMaker
+{
+    protected override void Bake()
+    {
+        Console.WriteLine("Baking the Whole Wheat Bread. (15 minutes)");
+    }
+
+    protected override void MixIngredients()
+    {
+        Console.WriteLine("Gathering Ingredients for Whole Wheat Bread.");
+    }
+}
