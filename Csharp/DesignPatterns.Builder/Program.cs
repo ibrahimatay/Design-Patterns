8cb0d8fe83719811f97436154ce548106f97bd14
ibrahimatay <contact@ibrahimatay.com>
2023-12-22 17:44:12 +0300

    DesignPatterns.Builder

---
+++ Csharp/DesignPatterns.Builder/Program.cs
@@ -0,0 +1,61 @@
+﻿String connectionString = new ConnectionBuilder()
+    .SetServer("127.0.0.1")
+    .SetDatabase("Northwind")
+    .SetUserId("Nancy")
+    .SetPassword("123456")
+    .Build();
+
+Console.WriteLine(connectionString);
+
+
+class ConnectionBuilder
+{
+    private class Connection
+    {
+        public string database;
+        public string server;
+        public string userId;
+        public string password;
+
+        public override string ToString()
+        {
+            return $"Server={server};Database={database};UserId={userId};Password={password}";
+        }
+    }
+
+    private readonly Connection connection;
+
+    public ConnectionBuilder()
+    {
+        connection = new Connection();
+    }
+
+    public ConnectionBuilder SetDatabase(String database)
+    {
+        connection.database = database;
+        return this;
+    }
+
+    public ConnectionBuilder SetServer(String server)
+    {
+        connection.server = server;
+        return this;
+    }
+
+    public ConnectionBuilder SetUserId(String userId)
+    {
+        connection.userId = userId;
+        return this;
+    }
+
+    public ConnectionBuilder SetPassword(String password)
+    {
+        connection.password = password;
+        return this;
+    }
+
+    public String Build()
+    {
+        return connection.ToString();
+    }
+}
