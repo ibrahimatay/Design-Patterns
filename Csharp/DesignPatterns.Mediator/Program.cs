4f8ff0823bb369e6a0a8fd7dbc2f1642fccd3032
ibrahimatay <contact@ibrahimatay.com>
2023-12-23 13:03:30 +0300

    DesignPatterns.Mediator

---
+++ Csharp/DesignPatterns.Mediator/Program.cs
@@ -0,0 +1,48 @@
+﻿IChat chat = new AnonymousChat();
+
+IMessenger bobMessenger = new BobMessenger();
+IMessenger aliceMessenger = new AliceMessenger();
+
+chat.AddMessenger(bobMessenger);
+chat.AddMessenger(aliceMessenger);
+
+chat.SendMessage(aliceMessenger, "Hello");
+chat.SendMessage(bobMessenger, "How are you, Alice");
+
+interface IMessenger
+{
+    void Receiver(string message);
+}
+
+interface IChat
+{
+    void SendMessage(IMessenger receiver, string message);
+    void AddMessenger(IMessenger messenger);
+}
+
+class AliceMessenger : IMessenger
+{
+    public void Receiver(string message) => Console.WriteLine($"received the message: {message}");
+}
+
+class BobMessenger : IMessenger
+{
+    public void Receiver(string message) => Console.WriteLine($"received the message: {message}");
+}
+
+class AnonymousChat : IChat
+{
+    private readonly List<IMessenger> _messengers = new();
+
+    public void SendMessage(IMessenger receiver, string message)
+    {
+        if (!string.IsNullOrWhiteSpace(message))
+        {
+            return;
+        }
+
+        receiver.Receiver(message);
+    }
+
+    public void AddMessenger(IMessenger messenger) => _messengers.Add(messenger);
+}
