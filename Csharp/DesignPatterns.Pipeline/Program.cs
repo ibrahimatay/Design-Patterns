03f6d34c1b715731b264288dd6387502f68c119c
ibrahimatay <contact@ibrahimatay.com>
2023-12-31 20:08:37 +0300

    DesignPatterns.Pipeline

---
+++ Csharp/DesignPatterns.Pipeline/Program.cs
@@ -0,0 +1,10 @@
+﻿using DesignPatterns.Pipeline.Pipelines;
+using DesignPatterns.Pipeline.Stage;
+
+var pipeline = new MessagesPipeline();
+pipeline.Add(new CreateMessagesStage());
+pipeline.Add(new RemoveDuplicatesStage());
+pipeline.Add(new SortMessagesStage());
+pipeline.Add(new OutputMessagesStage());
+
+pipeline.Execute();
