a0dc03c1fb762e45c76cf877b72df7c6c3382747
Atay <Ibrahim.Atay@sodexo.com>
2024-01-15 14:38:27 +0300

    DesignPatterns.SingletonEagerInstantiation

---
+++ Csharp/DesignPatterns.SingletonEagerInstantiation/Program.cs
@@ -0,0 +1,27 @@
+﻿
+
+DateTimeSnapshot instance = Singleton.GetInstance();
+
+Console.WriteLine(instance.GetDateTime());
+Thread.Sleep(1000);
+Console.WriteLine(instance.GetDateTime());
+Thread.Sleep(1000);
+Console.WriteLine(instance.GetDateTime());
+
+
+class DateTimeSnapshot
+{
+    public string GetDateTime()
+    {
+        return DateTime.Now.ToString();
+    }
+}
+
+class Singleton
+{
+    private readonly static DateTimeSnapshot instance = new();
+    public static DateTimeSnapshot GetInstance()
+    {
+        return instance;
+    }
+}
