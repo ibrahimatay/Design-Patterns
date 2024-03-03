1cf1fa4a649f1a016692f43444b7c45cf8eeeeae
İbrahim ATAY <atay363@gmail.com>
2024-03-03 19:50:40 +0000

    review singleton lazy instantiation

--- Java/singleton-lazy-instantiation/src/main/java/com/ibrahimatay/Main.java
+++ Java/singleton-lazy-instantiation/src/main/java/com/ibrahimatay/Main.java
@@ -1,24 +1,30 @@
 package com.ibrahimatay;
 
-import java.time.LocalDateTime;
+import java.util.Random;
 
-class DateTimeSnapshot {
-    public String getDateTime() {
-        return LocalDateTime.now().toString();
+class NumberGenerator {
+    private final long currentNumber;
+
+    public NumberGenerator() {
+        currentNumber = new Random().nextLong(1000);
+    }
+
+    public long getNumber() {
+        return currentNumber;
     }
 }
 
 final class Singleton {
-    private static DateTimeSnapshot instance;
+    private static NumberGenerator instance;
 
     Singleton() {
         System.out.println("Singleton being initialized");
     }
 
-    public static DateTimeSnapshot getInstance() {
+    public static NumberGenerator getInstance() {
         if(instance == null) {
             System.out.println("make a new instance...");
-            instance = new DateTimeSnapshot();
+            instance = new NumberGenerator();
         }
 
         return instance;
@@ -27,8 +33,8 @@ final class Singleton {
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(Singleton.getInstance().getDateTime());
-        System.out.println(Singleton.getInstance().getDateTime());
-        System.out.println(Singleton.getInstance().getDateTime());
+        System.out.println(Singleton.getInstance().getNumber());
+        System.out.println(Singleton.getInstance().getNumber());
+        System.out.println(Singleton.getInstance().getNumber());
     }
 }
